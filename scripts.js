3fcd07c2d2d80e320c4d2615bb1aa8a89058eeb2
Dels <deliarecio@gmail.com>
2025-12-11 10:35:04 +0100

    Hecho desplegable en cuotas con acordeón

--- scripts.js
+++ scripts.js
@@ -11,3 +11,16 @@ tabs.forEach(tab => tab.classList.remove('active'));
 document.getElementById(id).classList.add('active');
 });
 });
+
+document.querySelectorAll(".accordion-title").forEach(title => {
+    title.addEventListener("click", () => {
+        const content = title.nextElementSibling;
+        content.classList.toggle("open");
+    });
+});
+
+document.querySelectorAll(".flip-card").forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("flipped");
+  });
+});
